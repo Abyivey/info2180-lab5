92fd0912d689ad20f06716978259ba7120897337
Abygayle Ivey <abyivey@gmail.com>
2020-11-19 23:17:05 -0500

    Exercise 3

--- world.php
+++ world.php
@@ -1,17 +1,20 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
+$mys = 'is ruled by';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+$country = filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$coun = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+$results = $coun->fetchAll(PDO::FETCH_ASSOC);
+echo $country;
+
+
 
 ?>
-<ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?>
-</ul>
