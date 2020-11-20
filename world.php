b658c6f5e23837f5fbbfdae9c2c14b0a6d3df1b4
Abygayle Ivey <abyivey@gmail.com>
2020-11-19 23:49:19 -0500

    final final

--- world.php
+++ world.php
@@ -1,20 +1,74 @@
 <?php
+
 $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
-$mys = 'is ruled by';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$country = filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
 
-$coun = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-$results = $coun->fetchAll(PDO::FETCH_ASSOC);
-echo $country;
+$country = filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
+$context = filter_input(INPUT_GET, "context", FILTER_SANITIZE_STRING);
+$my_country = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+$results = $my_country->fetchAll(PDO::FETCH_ASSOC);
+
+
+$mi_city= $conn->query("SELECT cities.name, cities.district, cities.population
+FROM cities LEFT JOIN countries ON countries.code = cities.country_code
+WHERE countries.name LIKE '%$country%'");
+
+
+$city = $mi_city->fetchAll(PDO::FETCH_ASSOC);
 
 
 
 ?>
 <?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
+  <li><?= $row['name'] . 'is ruled by' .  $row['head_of_state']; ?></li>
+<?php endforeach;?> -->
+
+<?php if(isset($country)&&(!isset($context))):?>
+  <table class = "display">
+      <caption><h2>TABLE SHOWING COUNTRIES<h2></caption>
+    <thead>
+      <tr>
+          <th class = "th1">Name</th>
+          <th class = "th2">Continent</th>
+          <th class = "th3">Independence</th>
+          <th class = "th4">Name of State</th>
+      </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($results as $country): ?>
+          <tr>
+            <td><?php echo $country["name"]; ?></td>
+            <td><?php echo $country["continent"]; ?></td>
+            <td><?php echo $country["independence_year"]; ?></td>
+            <td><?php echo $country["head_of_state"]; ?></td>
+          </tr>
+        <?php endforeach; ?>
+    </tbody>
+  </table>
+<?php endif; ?>
+
+<?php if (isset($context)):?>
+  <table class = "display">
+    <caption><h2>TABLE SHOWING CITIES</h2></caption>
+    <thead>
+      <tr>
+        <th class = "cth1">Name</th>
+        <th class = "cth2">District</th>
+        <th class = "cth3">Population</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($city as $city): ?>
+        <tr>
+          <td><?php echo $city["name"]; ?></td>
+          <td><?php echo $city["district"]; ?></td>
+          <td><?php echo $city["population"]; ?></td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+<?php endif ?>
